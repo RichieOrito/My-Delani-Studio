fb994688f6cc879f73931b5c2769041bce96d549
RichieOrito <richard.omondi@student.moringaschool.com>
2021-11-21 19:19:18 +0300

    This commit adds jquery to the portfolio hover effects

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
   $('#design-details, #dev-details, #mgt-details').hide();
+  
   $('#design-img').click(function(){
     $('#design-img').hide();
     $('#design-details').show();
@@ -25,4 +26,56 @@ $(document).ready(function(){
     $('#mgt-details').hide();
     $('#mgt-img').show();
   });
+
+  $('#proj1, #proj2, #proj3, #proj4, #proj5, #proj6, #proj7, #proj8').hide();
+
+  $('#port1').hover(function(){
+    $('#proj1').fadeIn();
+  }, function(){
+    $('#proj1').fadeOut();
+  });
+
+  $('#port2').hover(function(){
+    $('#proj2').fadeIn();
+  }, function(){
+    $('#proj2').fadeOut();
+  });
+
+  $('#port3').hover(function(){
+    $('#proj3').fadeIn();
+  }, function(){
+    $('#proj3').fadeOut();
+  });
+
+  $('#port4').hover(function(){
+    $('#proj4').fadeIn();
+  }, function(){
+    $('#proj4').fadeOut();
+  });
+
+  $('#port5').hover(function(){
+    $('#proj5').fadeIn();
+  }, function(){
+    $('#proj5').fadeOut();
+  });
+
+  $('#port6').hover(function(){
+    $('#proj6').fadeIn();
+  }, function(){
+    $('#proj6').fadeOut();
+  });
+
+  $('#port7').hover(function(){
+    $('#proj7').fadeIn();
+  }, function(){
+    $('#proj7').fadeOut();
+  });
+
+  $('#port8').hover(function(){
+    $('#proj8').fadeIn();
+  }, function(){
+    $('#proj8').fadeOut();
+  });
+
+  
 });
